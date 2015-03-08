2aa3cb790d0febee569dc2aafdb7cd2717ecb878
Jonathan Wesley Stone <js@piratejon.com>
2015-03-08 12:15:28 -0500

    add usage comments to subset script

--- subset.sh
+++ subset.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+### usage: ./subset.sh inputfilename outputfilename
+### example: ./subset.sh household_power_consumption.txt household_subset.txt
+
 head -n 1 ${1} > ${2}
 sed '/^[12]\/2\/2007;/!d' < ${1} >> ${2}
 
