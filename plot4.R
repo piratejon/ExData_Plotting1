979c53f6e0e296b5e46591975e20e240a6d9e255
Jonathan Wesley Stone <js@piratejon.com>
2015-03-08 12:07:47 -0500

    working plot 4

--- plot4.R
+++ plot4.R
@@ -1,21 +1,19 @@
 
 ### Exploratory data analysis project 1 plot 4
 
-### lattice plot of global active power by date, voltage by date, submetering by date and color, and global reactive power by date
+### small multiples of global active power by date, voltage by date, submetering by date and color, and global reactive power by date
 
 source('load_data.R')
 data <- load_data()
 
-#####plot(y=Global_active_power, x=Datetime, ylab="Global Active Power (kilowatts)", xlab="", type="l")
-
+par(mfrow=c(2,2))
 with(data, {
-  plot(Datetime, Global_active_power, type="l")
-  })
-#  plot(Datetime, Voltage, xlab="datetime", ylab="Voltage", type="l")
-#  plot(Datetime, Sub_metering_1, type="l", col="black", ylab="Energy sub metering", xlab="")
-#  lines(Datetime, Sub_metering_2, type="l", col="red")
-#  lines(Datetime, Sub_metering_3, type="l", col="blue")
-#  legend(x="topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
-#  plot(x=Datetime, y=Global_reactive_power, type="l")
-#})
-#
+  plot(Datetime, Global_active_power, type="l", ylab="Global Active Power", xlab="")
+  plot(Datetime, Voltage, type="l", xlab="datetime")
+  plot(Datetime, Sub_metering_1, type="l", col="black", ylab="Energy sub metering", xlab="")
+  lines(Datetime, Sub_metering_2, type="l", col="red")
+  lines(Datetime, Sub_metering_3, type="l", col="blue")
+  legend(x="topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+  plot(Datetime, Global_reactive_power, type="l", xlab="datetime")
+})
+
