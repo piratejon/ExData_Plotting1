9a219a02ccc7b30223f3fc84f53aeaaf76743eda
Jonathan Wesley Stone <js@piratejon.com>
2015-03-08 10:50:14 -0500

    display energy submetering by date in color

---
+++ plot3.R
@@ -0,0 +1,15 @@
+
+### Exploratory Data Analysis project 1 plot 3
+
+### submetering 1, 2, and 3 by date, in different colors
+
+data <- read.csv('household_subset.txt', sep=';')
+data$Datetime <- strptime(sprintf("%s %s", data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+
+png("./plot3.png")
+plot(x=data$Datetime, y=data$Sub_metering_1, type="l", col="black", ylab="Energy sub metering", xlab="")
+lines(x=data$Datetime, y=data$Sub_metering_2, type="l", col="red")
+lines(x=data$Datetime, y=data$Sub_metering_3, type="l", col="blue")
+legend(x="topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty=1)
+dev.off()
+
